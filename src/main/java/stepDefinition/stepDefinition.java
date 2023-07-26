69bd15b567f32410ffdced8b994e4a429ecdd3e0
Momita Bogdan-Andrei <bmomita@deloittece.com>
2023-07-26 11:10:46 +0300

    wip

---
+++ src/main/java/stepDefinition/stepDefinition.java
@@ -0,0 +1,19 @@
+package stepDefinition;
+
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class stepDefinition {
+
+    WebDriver driver;
+
+    @Before
+            public void Before {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+}
---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,10 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Utils {
+    WebDriver driver;
+    WebDriverWait wait;
+
+}
